a5ac028a55343531b435f767a721da0b926fd714
sidali-djeghbal <sidalidjeghbal@gmail.com>
2026-05-24 19:15:40 +0100

    Refactor code structure for improved readability and maintainability

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "بنيان";
-export const SITE_DESCRIPTION = "بنيان صرح تطوعي يجمع شبابًا مسلمًا لصناعة أثر نافع وبناء مجتمع واعٍ.";
+export const SITE_DESCRIPTION =
+	" منصة تعريفية لروابط مشروع بنيان تجمع أهم الروابط الرسمية وتقدّم لمحة سريعة عن الفروع والوسائل المعتمدة للتواصل.";
+export const SITE_SOCIAL_DESCRIPTION =
+	"هنا تجد حسابات التواصل، التعريف بالفروع، ووسائل الوصول إلى المحتوى والمستجدات في مكان واحد.";
